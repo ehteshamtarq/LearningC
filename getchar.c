696b1704869357faa17cf8a7671d726383ebd8e4
ehteshamtarq <ehteshamtarique1@gmail.com>
2024-07-08 13:06:42 +0530

    Day 1 - Learning C

--- getchar.c
+++ getchar.c
@@ -1,18 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-// #define EOF -1
 main(){
-    // char ch = 'H';
-    // int length = strlen(ch);
-
-    // for(int i = 0; i < length / 2; i++){
-    //     char temp = ch[i];
-    //     ch[i] = ch[length-i-1];
-    //     ch[length-i-1] = temp;
-    // }
-
-    // float f = 3.2;
-    // printf("%.3f", f);
 
     int c = getchar();
     while ( c != -1){
